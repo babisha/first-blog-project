1226ee670a916f0e5f54d93cd5296d85b61a17d9
Babisha Manandhar <babishamanandhar@Babishas-MacBook.local>
2018-05-10 18:18:40 -0400

    Initial commit

---
+++ loops.js
@@ -0,0 +1,41 @@
+// var counter = -10;
+//
+// while(counter < 20){
+//   console.log(counter);
+//   counter++;
+// }
+//
+// var counter = 10;
+//
+// while(counter <= 40){
+//   if(counter % 2 === 0){
+//     console.log(counter);
+//   }
+//   counter+=1;
+// }
+//
+// var counter = 300;
+//
+// while(counter <= 333){
+//   if(counter % 2 !== 0){
+//     console.log(counter);
+//   }
+//   counter+=1;
+// }
+
+ var str = "hello";
+
+ for(var i = 0; i < str.length; i++){
+   console.log(str[0]);
+ }
+
+FOR loops
+ for(var i = -10; i < 20; i++){
+   console.log(i)
+ }
+
+for(var i = 10; i < 40; i+= 1){
+  if(i % 2 === 0) {
+    console.log(i);
+  }
+}
